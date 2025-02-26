cf7a8a4c0220428747feaeb3f7aec61ef25c7ace
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-02-26 20:10:24 +0000

    Created initial asp.net projecct

---
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.DataModels/Tables/AccountTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingWFMSApi.DataModels.Tables
+{
+    [Table("account")]
+    public class AccountTable
+    {
+        [Key]
+        public string Id { get; set; }
+        public string DisplayName { get; set; }
+        public string EmailAddress { get; set; }
+        public string HashedPassword { get; set; }
+        public bool IsOnboarder { get; set; }
+        public bool IsAdmin { get; set; }
+        public string DepartmentId { get; set; }
+        public string OrganisationId { get; set; }
+    }
+}
