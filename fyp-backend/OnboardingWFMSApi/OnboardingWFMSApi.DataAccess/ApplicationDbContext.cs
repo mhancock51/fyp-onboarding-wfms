e18f975760f148d74f37eb50256f7f3993ea5bef
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-02-27 11:31:00 +0000

    Created compose file to provision MySQL server and made a schema sql file with table creation scripts. Created DbContext

---
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnboardingWFMSApi.DataModels.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingWFMSApi.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<OrganisationTable> Organisations { get; set; }
+        public DbSet<AccountTable> Accounts { get; set; }
+        public DbSet<DepartmentTable> Departments { get; set; }
+        public DbSet<OrganisationAdminLinkTable> OrganisationAdmins { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
--- fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.Presentation/Program.cs
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.Presentation/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
