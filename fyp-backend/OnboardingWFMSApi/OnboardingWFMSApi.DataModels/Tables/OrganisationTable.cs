8ca5564ef2eb95441336192021a3aecfe93d97b6
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-02-27 11:31:36 +0000

    Created table classes for each table

---
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.DataModels/Tables/OrganisationTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingWFMSApi.DataModels.Tables
+{
+    [Table("organisation")]
+    public class OrganisationTable
+    {
+        [Key]
+        public string OrganisationId { get; set; }
+        public string Name { get; set; }        
+    }
+}
