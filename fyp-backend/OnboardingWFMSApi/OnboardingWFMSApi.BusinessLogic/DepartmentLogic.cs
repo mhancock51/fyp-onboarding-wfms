f02d48068f75fa6376cd86b1aa5796e9f125c405
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-02-27 11:32:03 +0000

    Implemented base repository Implemented logic and endpoint to create a department

---
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.BusinessLogic/DepartmentLogic.cs
@@ -0,0 +1,32 @@
+﻿using OnboardingWFMSApi.DataAccess.Repositories;
+using OnboardingWFMSApi.DataModels;
+using OnboardingWFMSApi.DataModels.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingWFMSApi.BusinessLogic
+{
+    public interface IDepartmentLogic
+    {
+        Task<HTTPResponse<string, string>> CreateDepartment(string name);
+    }
+
+    public class DepartmentLogic : IDepartmentLogic
+    {
+        readonly private IDepartmentRepository _departmentRepository;
+
+        public DepartmentLogic(IDepartmentRepository departmentRepository)
+        {
+            _departmentRepository = departmentRepository;
+        }
+
+        public async Task<HTTPResponse<string, string>> CreateDepartment(string name)
+        {
+            await _departmentRepository.AddAsync(new DepartmentTable() { DisplayName = name });
+            return new HTTPResponse<string, string> { Success = true, HttpCode = 200, Data = "Created department" };
+        }
+    }
+}
---
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.DataAccess/Repositories/BaseRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnboardingWFMSApi.DataAccess.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        public Task AddAsync(TEntity entity);
+        public Task DeleteAsync(TEntity entity);
+        public Task UpdateAsync(TEntity entity);
+    }
+
+    public class BaseRepository<TEntity> : IRepository<TEntity>
+    {
+        protected readonly ApplicationDbContext _dbContext;
+
+        public static string GenerateRandomId()
+        {
+            var random = new Random();
+            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var length = 8;
+            var result = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = characters[random.Next(characters.Length)];
+            }
+            return new String(result);
+        }
+
+        public BaseRepository(ApplicationDbContext dbContext) 
+        {
+            _dbContext = dbContext;    
+        }
+
+        public virtual async Task AddAsync(TEntity entity)
+        {            
+            var result = await _dbContext.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public virtual async Task DeleteAsync(TEntity entity)
+        {
+            var result = _dbContext.Remove(entity);
+        }
+
+        public virtual async Task UpdateAsync(TEntity entity)
+        {
+            var result = _dbContext.Update(entity);
+        }
+    }
+}
