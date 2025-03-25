d06829da5fc4610bd76a71fef11c8ebf75eaba0b
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-03-25 16:21:43 +0000

    Removed old endpoints

--- fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.Presentation/Controllers/TaskInstanceController.cs
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.Presentation/Controllers/TaskInstanceController.cs
@@ -60,57 +60,6 @@ namespace OnboardingWFMSApi.Presentation.Controllers
             }
         }
 
-        [Authorize]
-        [HttpPost("update-task-state/checklist")]
-        public async Task<IActionResult> UpdateChecklistInstanceState([FromBody] UpdateInstanceStateChecklistPayload payload)
-        {
-            string accountId = UserIdentityUtils.GetAccountIdFromClaimIdentity(User.Identity as ClaimsIdentity);
-            if (accountId == "")
-            {
-                var response = new HTTPResponse<string, string>() { Success = false, HttpCode = 401, Message = "Invalid credentials" };
-                return StatusCode(response.HttpCode, response);
-            }
-            else
-            {
-                var response = await _taskInstanceLogic.UpdateChecklistInstanceState(payload, accountId);
-                return StatusCode(response.HttpCode, response);
-            }            
-        }
-
-        [Authorize]
-        [HttpPost("update-task-state/read-doc")]
-        public async Task<IActionResult> UpdateReadDocInstanceState([FromBody] UpdateInstanceStateReadDocPayload payload)
-        {
-            string accountId = UserIdentityUtils.GetAccountIdFromClaimIdentity(User.Identity as ClaimsIdentity);
-            if (accountId == "")
-            {
-                var response = new HTTPResponse<string, string>() { Success = false, HttpCode = 401, Message = "Invalid credentials" };
-                return StatusCode(response.HttpCode, response);
-            }
-            else
-            {
-                var response = await _taskInstanceLogic.UpdateReadDocumentInstanceState(payload, accountId);
-                return StatusCode(response.HttpCode, response);
-            }
-        }
-
-        [Authorize]
-        [HttpPost("update-task-state/upload-doc")]
-        public async Task<IActionResult> UpdateUploadDocInstanceState([FromForm] UpdateInstanceStateFileUploadPayload payload)
-        {
-            string accountId = UserIdentityUtils.GetAccountIdFromClaimIdentity(User.Identity as ClaimsIdentity);
-            if (accountId == "")
-            {
-                var response = new HTTPResponse<string, string>() { Success = false, HttpCode = 401, Message = "Invalid credentials" };
-                return StatusCode(response.HttpCode, response);
-            }
-            else
-            {
-                var response = await _taskInstanceLogic.UpdateFileUploadInstanceState(payload, accountId);
-                return StatusCode(response.HttpCode, response);
-            }
-        }
-
         [HttpPost("complete")]
         public async Task<IActionResult> CompleteInstance(string taskInstanceId)
         {
