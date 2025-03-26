731d440346e2871e243524503d89a9759ec4990e
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-03-26 18:59:31 +0000

    Fixed logic of displaying the loading message in the app's route

--- fyp-frontend/src/App.tsx
+++ fyp-frontend/src/App.tsx
@@ -12,16 +12,16 @@ import { RootState, store } from './store';
 import { useEffect, useState } from 'react';
 import Api from './api';
 import Utils from './util';
-import { Button } from './components/ui/button';
 import RegisterPage from './app/pages/RegisterPage/RegisterPage';
 import MyTasksPage from './app/pages/MyTasksPage/MyTasksPage';
 import { Spinner } from './components/ui/spinner';
 import { SET_ACCOUNTS_DIRECTORY, SET_TASK_TEMPLATES, SET_TASK_TYPES } from './features/appSlice';
-import TaskType from './models/tasks/taskType';
+import TaskType from './models/tasks/TaskType';
 import AccountDirectory from './models/AccountDirectory';
 import WorkflowInstancesPage from './app/pages/WorkflowInstancesPage/WorkflowInstancesPage';
+import WorkflowDashboardPage from './app/pages/WorkflowDashboardPage/WorkflowDashboardPage';
 
-function App() {  
+export default function App() {  
 
   const user = useSelector((state: RootState) => state.app.user);  
   const taskTypes = useSelector((state: RootState) => state.app.taskTypes);
@@ -116,32 +116,49 @@ function App() {
 
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>    
-      {
-        (validatedToken && loadedAccounts && loadedTaskTypes && loadedTaskTemplates) || user === null ? (
-          <Router>
-            <Routes>
-              <Route element={user !== null ? <Layout/> : <Navigate to={"/login"} />}>
-                <Route path="/" element={<MyTasksPage/>}/>                
-                <Route path="/workflows" element={<WorkflowInstancesPage/>} />                
-                <Route path="/workflow-builder" element={<CreateWorkflowPage/>} />            
-                <Route path="/settings" element={<div><h1>Settings</h1></div>} />                
-              </Route>
-              <Route path="/login" element={user === null ? <LoginPage/> : <Navigate to={"/"}/>}/>          
-              <Route path='/register' element={user === null ? <RegisterPage/> : <Navigate to={"/"}/>}/>
-            </Routes>
-          </Router>         
-        ) : (
-          <div className='flex flex-col justify-center my-auto' style={{minHeight: "100vh"}}>
-            <div className='flex flex-row gap-2 justify-center'>
-              <h1>Authenticating...</h1>
-              <Spinner/>
-            </div>
-          </div>
-        )
-      }  
-      <Toaster /> 
-    </ThemeProvider>    
-  )
+      <Router>
+        <Routes>
+          {(validatedToken && loadedAccounts && loadedTaskTypes && loadedTaskTemplates) || user === null ? (
+            <Route 
+              path="/" 
+              element={user !== null ? <Layout /> : <Navigate to="/login" />}
+            >
+              <>
+                <Route path="/" element={<MyTasksPage />} />
+                <Route path="/workflows" element={<WorkflowInstancesPage />} />
+                <Route path="/workflows/dashboard" element={<WorkflowDashboardPage />} />
+                <Route path="/workflow-builder" element={<CreateWorkflowPage />} />
+                <Route path="/settings" element={<div><h1>Settings</h1></div>} />
+              </>
+            </Route>
+          ) : (
+            <Route index element={<AppLoading validatedToken={validatedToken} 
+              loadedAccounts={loadedAccounts} loadedTaskTypes={loadedTaskTypes} loadedTaskTemplates={loadedTaskTemplates}/>}
+            />
+          )}
+          <Route path="/login" element={user === null ? <LoginPage /> : <Navigate to="/" />} />
+          <Route path="/register" element={user === null ? <RegisterPage /> : <Navigate to="/" />} />
+        </Routes>
+      </Router> 
+      <Toaster />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export function AppLoading(props: {validatedToken: boolean, loadedAccounts: boolean, loadedTaskTypes: boolean, loadedTaskTemplates: boolean}) {
+  return (
+    <div className="flex flex-col justify-center my-auto" style={{ minHeight: "100vh" }}>
+      <div className="flex flex-row gap-2 justify-center">
+        {
+          !props.validatedToken &&
+          <h1>Authenticating...</h1>
+        }
+        {
+          props.validatedToken && (!props.loadedAccounts || !props.loadedTaskTypes || !props.loadedTaskTemplates) &&
+          <h1>Loading...</h1>
+        }
+        <Spinner />
+      </div>
+    </div>
+  )
+}
