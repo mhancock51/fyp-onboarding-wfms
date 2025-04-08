c92d05f359d8cfd2af1f0fadee405248e836a84e
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-04-08 15:23:17 +0100

    Implemented endpoint to retrieve notifications Implemented handler to create notifications through a mediator

---
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.Presentation/Controllers/NotificationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnboardingWFMSApi.BusinessLogic;
+using OnboardingWFMSApi.DataModels;
+using System.Security.Claims;
+
+namespace OnboardingWFMSApi.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/notifications")]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationLogic _notificationLogic;
+
+        public NotificationController(INotificationLogic notificationLogic)
+        {
+            _notificationLogic = notificationLogic;
+        }
+
+        [Authorize]
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllNotifications()
+        {
+            string accountId = UserIdentityUtils.GetAccountIdFromClaimIdentity(User.Identity as ClaimsIdentity);
+            if (accountId == "")
+            {
+                var response = new HTTPResponse<string, string>() { Success = false, HttpCode = 401, Message = "Invalid credentials" };
+                return StatusCode(response.HttpCode, response);
+            }
+            else
+            {
+                var response = await _notificationLogic.GetAccountsNotification(accountId);
+                return StatusCode(response.HttpCode, response);
+            }
+        }
+    }
+}
