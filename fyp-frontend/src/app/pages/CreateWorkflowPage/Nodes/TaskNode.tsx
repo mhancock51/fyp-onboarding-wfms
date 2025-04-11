ba3dc0b75463a6c882773fb59e0e3163ffe3bced
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-04-11 11:34:17 +0100

    removed redudant hover card that displays node's dependencies

--- fyp-frontend/src/app/pages/CreateWorkflowPage/Nodes/TaskNode.tsx
+++ fyp-frontend/src/app/pages/CreateWorkflowPage/Nodes/TaskNode.tsx
@@ -90,27 +90,12 @@ export default function TaskNode(props: NodeProps<TaskNode>) {
             </div>
             <Separator/>
             <div className='flex flex-row gap-2 py-1 justify-center w-full items-center'>
+              <TaskTypeBadge taskTypeId={props.data.taskTypeId}/>
               <AccountDirectoryBadge accountDirectory={props.data.assignee}/>              
               {
                 props.data.taskDependencies.length > 0 &&
-                <HoverCard>
-                  <HoverCardTrigger>
-                    <Badge className='rounded-full py-2 px-4'>{props.data.taskDependencies.length} Dependencies</Badge>
-                  </HoverCardTrigger> 
-                  <HoverCardContent side='top' className='p-1' style={{boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px"}}>
-                    <div className='flex flex-col gap-1 text-sm'>
-                      <Label>{props.data.taskDependencies.length > 0 ? "Dependencies" : "Dependency"}</Label>
-                      <Separator/>
-                    {
-                      props.data.taskDependencies.map((dependency, index) => (
-                        <Label key={index} className='text-xs cursor-pointer'>{dependency.taskTemplate?.name}</Label>
-                      ))
-                    }
-                    </div>
-                  </HoverCardContent>
-                </HoverCard>
+                <Badge className='rounded-full py-2 px-4'>{props.data.taskDependencies.length} Dependencies</Badge>
               }             
-              <TaskTypeBadge taskTypeId={props.data.taskTypeId}/>
               {
                 props.data.daysUntilDue !== null && props.data.daysUntilDue !== undefined &&
                 <Badge className='rounded-full py-2 px-4'>{props.data.daysUntilDue} Days</Badge>
