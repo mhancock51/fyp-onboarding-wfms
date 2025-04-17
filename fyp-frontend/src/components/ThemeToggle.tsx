407db5f98b783ac7ca8c25b88e40798d3c1e9bc7
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-04-17 17:33:59 +0100

    Improved theme toggle styling

--- fyp-frontend/src/components/ThemeToggle.tsx
+++ fyp-frontend/src/components/ThemeToggle.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Button } from './ui/button';
 import { toast } from 'sonner';
 import { useTheme } from 'next-themes';
+import { Moon, Sun } from 'lucide-react';
 
 export default function ThemeToggle() {  
   const { theme, setTheme} = useTheme();  
+
+  const [hovered, setHovered] = useState<boolean>(false);
 
   function toggleTheme() {    
     setTheme(theme === "light" ? "dark" : "light");  
@@ -12,9 +15,26 @@ export default function ThemeToggle() {
   }
 
   return (
-    <div className='flex flex-row gap-2 items-center'>
-      <Button onClick={toggleTheme}>Toggle Theme</Button>
-      <span>{theme}</span>
+    <div className='flex flex-row gap-2 items-center w-full' 
+      onMouseEnter={() => {setHovered(true)}} onMouseLeave={() => {setHovered(false)}}
+    >
+      {
+        theme === "light" && !hovered &&
+        <Sun/>
+      }
+      {
+        theme === "light" && hovered &&
+        <Moon/>
+      }
+      {
+        theme == "dark" && !hovered &&
+        <Moon/>
+      }
+      {
+        theme == "dark" && hovered &&
+        <Sun/> 
+      }
+      <a className='rounded-xl' onClick={toggleTheme}>Toggle Theme</a>
     </div>
   )
 }
