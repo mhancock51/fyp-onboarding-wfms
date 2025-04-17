39fabda27f330ca557fcad9555becc4be3040f83
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-04-17 16:12:08 +0100

    Implemented separate "task instances" table and extended tasks page to put tasks in separate tables

--- fyp-frontend/src/app/pages/MyTasksPage/MyTasksPage.tsx
+++ fyp-frontend/src/app/pages/MyTasksPage/MyTasksPage.tsx
@@ -1,18 +1,14 @@
-import { Badge } from '@/components/ui/badge'
 import { Separator } from '@/components/ui/separator'
-import { Table, TableBody, TableCell, TableHeader, TableRow } from '@/components/ui/table'
 import TaskInstanceDTO from '@/models/tasks/TaskInstanceDTO'
 import React, { useEffect, useState } from 'react'
 import TaskDrawer from './TaskDrawer/TaskDrawer'
-import TaskTypeBadge from './TaskTypeBadge'
 import Api from '@/api'
 import { toast } from 'sonner'
-import { Spinner } from '@/components/ui/spinner'
-import NoResults from '@/components/NoResults'
 import { useDispatch } from 'react-redux'
-import TaskStatusBadge from './TaskStatusBadge'
 import ReportIssueDialog from '@/app/dialogs/ReportIssueDialog'
-import WorkflowInstanceBadge from '@/components/WorkflowInstanceBadge'
+import TaskInstancesTable from '@/components/Tables/TaskInstancesTable'
+import { Accordion, AccordionItem, AccordionTrigger } from '@/components/ui/accordion'
+import { AccordionContent } from '@radix-ui/react-accordion'
 
 export default function MyTasksPage() {
   const dispatcher = useDispatch(); 
@@ -36,124 +32,38 @@ export default function MyTasksPage() {
     })
   }
 
-  function dueInColor(dueIn: number | null) {
-    if (dueIn === null) {
-      return "bg-accent"
-    }
-    if (dueIn < 2) {
-      return "bg-red-600";
-    }
-    else if (dueIn < 4) {
-      return "bg-orange-500";
-    }
-    else {
-      return "bg-green-600";
-    }
-  }  
-
-  function dueDisplayValue(dueIn: number | null) {
-    if (dueIn === null) return "N/A";
-    if (dueIn < 0) return `Overdue (${dueIn * -1} days)`;
-    if (dueIn === 0) return "Today";
-    if (dueIn === 2) return "Tomorrow";
-    return `${dueIn} days`;
-  }
-
-  function daysUntil(date: Date | null): number | null {
-    if (date === null) return null;
-    const targetDate = new Date(date);
-    if (isNaN(targetDate.getTime())) {
-      throw new Error("Invalid date provided");
-    }
-    const today = new Date();
-    today.setHours(0, 0, 0, 0); // normalize to midnight
-    targetDate.setHours(0, 0, 0, 0);  // normalize to midnight
-  
-    const diffTime = targetDate.getTime() - today.getTime();
-    return Math.ceil(diffTime / (1000 * 60 * 60 * 24)); // convert to days
-  }
-
   useEffect(() => {
     void fetchTaskInstances();
   }, []);
 
   return (
     <div>
-      <div>
-        <h1 className='text-xl text-foreground font-bold m-2'>Your Tasks ({tasks.filter(t => t.status === "open").length} open)</h1>
-        <Separator/>
-        {
-          loading && tasks.length === 0 &&
-          <div className='flex flex-row justify-center p-4 gap-2'>
-            <Spinner/>
-            <h1>Loading tasks...</h1>
-          </div>
-        }
-        {
-          !loading && tasks.length == 0 &&
-          <NoResults text='No tasks could be found'/>                    
-        }
-        {
-          !loading && tasks.length > 0 &&
-          <Table className='text-base'>
-            <TableHeader>
-              <TableCell style={{textAlign: "center"}}>Name    </TableCell>
-              <TableCell style={{textAlign: "center"}}>Type    </TableCell>
-              <TableCell style={{textAlign: "center"}}>Status  </TableCell>
-              <TableCell style={{textAlign: "center"}} width={100}>Due  </TableCell>
-              <TableCell style={{textAlign: "center"}}>Workflow</TableCell>
-              <TableCell style={{textAlign: "center"}}>Description</TableCell>
-            </TableHeader> 
-            <TableBody>
-              {
-                tasks.sort((a, b) => (new Date(a.creationTimestamp).getTime() - new Date(b.creationTimestamp).getTime())).map((task, index) => (
-                  <TableRow key={index} onClick={() => { setCurrentTask(task); setOpen(true); }} className='cursor-pointer'>
-                    <TableCell style={{maxWidth: "150px", overflowX: "hidden", textOverflow: "ellipsis"}}>
-                      {task.template.name}
-                    </TableCell>
-                    <TableCell width={"175px"}>                    
-                      <TaskTypeBadge taskTypeId={task.template.taskTypeId}/>
-                    </TableCell>
-                    <TableCell width={"100px"}>
-                      <TaskStatusBadge status={task.status}/>                      
-                    </TableCell>
-                    <TableCell width={"50px"}>
-                      {
-                        task.dueDate === null || task.status.toLowerCase() === "complete" &&
-                        <div className='text-foreground w-full flex flex-row justify-center text-xs'>
-                          N/A
-                        </div>
-                      }
-                      {
-                        task.dueDate !== null && task.status.toLowerCase() !== "complete" &&
-                        <Badge className={`mx-2 py-2 px-4 rounded-full w-full ${dueInColor(daysUntil(task.dueDate))}`}> 
-                          {
-                            dueDisplayValue(daysUntil(task.dueDate))
-                          }                                      
-                        </Badge>
-                      }
-                    </TableCell>
-                    <TableCell width={"100px"}>
-                      {
-                        task.workflowInstanceTemplateName === "" &&
-                        <div className='text-foreground w-full flex flex-row justify-center'>
-                          N/A
-                        </div>
-                      }
-                      {
-                        task.workflowInstance !== null &&
-                        <WorkflowInstanceBadge workflowInstance={task.workflowInstance}/>
-                      }
-                    </TableCell>
-                    <TableCell style={{maxWidth: "200px", overflowX: "hidden", textOverflow: "ellipsis"}}>
-                      {task.template.description}
-                    </TableCell>
-                  </TableRow>
-                ))
-              }        
-            </TableBody>
-          </Table>
-        }
+      <div className='flex flex-col gap-2'>
+        <h1 className='text-xl text-foreground font-bold'>Your Tasks ({tasks.filter(t => t.status === "open").length} open)</h1>
+        <div className='flex flex-col gap-1 h-full'>
+          <TaskInstancesTable tasks={tasks.filter(t => t.status === "open")} loading={loading} 
+            handleTaskClicked={(task: TaskInstanceDTO) => {setCurrentTask(task); setOpen(true);}}
+            className='h-[100%] overflow-y-auto'
+          />
+          {/* Completed tasks accordian */}
+          <Accordion type="single" collapsible className="w-full flex-2">
+            <AccordionItem value={'item-1'}>
+              <AccordionTrigger>
+                <div className='flex flex-col gap-2 w-full'>
+                  <h2>Completed Tasks ({tasks.filter(t => t.status !== "open").length})</h2>
+                  <Separator/>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent>
+                <TaskInstancesTable tasks={tasks.filter(t => t.status !== "open")} loading={loading} 
+                  handleTaskClicked={(task: TaskInstanceDTO) => {setCurrentTask(task); setOpen(true);}}
+                  className='h-[100%] overflow-y-auto'
+                  hideDueDate={true}
+                />
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
       </div>
       {
         currentTask !== null &&
