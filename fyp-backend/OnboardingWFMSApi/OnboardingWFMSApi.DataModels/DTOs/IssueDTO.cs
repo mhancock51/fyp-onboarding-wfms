95f95827577a9628ee573d7b41f761c423eb38ce
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2025-04-24 17:23:33 +0100

    Removed used references on backend

--- fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.DataModels/DTOs/IssueDTO.cs
+++ fyp-backend/OnboardingWFMSApi/OnboardingWFMSApi.DataModels/DTOs/IssueDTO.cs
@@ -1,5 +1,4 @@
-﻿using Newtonsoft.Json;
-using OnboardingWFMSApi.DataModels.Models;
+﻿using OnboardingWFMSApi.DataModels.Models;
 using OnboardingWFMSApi.DataModels.Tables;
 using System;
 using System.Collections.Generic;
