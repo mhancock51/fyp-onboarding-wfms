fba6deef6d92a473c914c4ee5e290e1355a9d7d8
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2026-07-19 17:20:03 +0100

    Cleaned up syntax

--- fyp-frontend/src/app/pages/WorkflowDashboardPage/DataCard.tsx
+++ fyp-frontend/src/app/pages/WorkflowDashboardPage/DataCard.tsx
@@ -2,6 +2,7 @@ import { Badge } from '@/components/ui/badge'
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 import { Spinner } from '@/components/ui/spinner';
 import { TrendingDownIcon, TrendingUpIcon, TriangleAlert } from 'lucide-react'
+import { JSX } from 'react';
 
 interface Props {
   label: string;
@@ -16,50 +17,50 @@ interface Props {
   errored?: boolean;
 }
 
-const DataCard = (): React.FC => {
+const DataCard = ({label, data, loading, trend, trendUp, fontSize, fontBold, colSpan, rowSpan, errored }: Props) => {
   return (
-    <Card className={`@container/card min-h-[150px] ${props.colSpan ? props.colSpan : ""} ${props.rowSpan ? props.rowSpan : ""}`}>
+    <Card className={`@container/card min-h-[150px] ${colSpan ? colSpan : ""} ${rowSpan ? rowSpan : ""}`}>
       <CardHeader className="relative">
-        <CardDescription>{props.label}</CardDescription>
-        <CardTitle className={`${props.fontSize ? props.fontSize : "text-3xl"} ${props.fontBold ? props.fontBold : "font-semibold"} tabular-nums`}>
+        <CardDescription>{label}</CardDescription>
+        <CardTitle className={`${fontSize ? fontSize : "text-3xl"} ${fontBold ? fontBold : "font-semibold"} tabular-nums`}>
           {
-            !props.loading && props.errored && 
+            !loading && errored && 
             <div className='w-full flex flex-row justify-start gap-2 items-center text-red-700 text-xl'>
               <TriangleAlert/>
               Failed to load
             </div>
           }
           {
-            props.loading &&
+            loading &&
             <div className='w-full flex flex-row justify-start'>
               <Spinner/>
             </div>
           }
           {
-            !props.loading && !props.errored && 
-            props.data
+            !loading && !errored && 
+            data
           }          
         </CardTitle>
         {
-          props.trend !== undefined && props.trendUp !== undefined &&
+          trend !== undefined && trendUp !== undefined &&
           <div className="absolute right-4 top-0">
             <Badge variant="outline" className="flex gap-1 rounded-lg text-xs">
               {
-                props.trendUp ? (
+                trendUp ? (
                   <TrendingUpIcon className="size-3" />
                 ) : (
                   <TrendingDownIcon className='size-3'/>
                 )
               }
               {
-                props.trendUp ? (
+                trendUp ? (
                   "+"
                 ) : (
                   "-"
                 )
               }
               {
-                `${props.trend}%`
+                `${trend}%`
               }
             </Badge>
           </div>
