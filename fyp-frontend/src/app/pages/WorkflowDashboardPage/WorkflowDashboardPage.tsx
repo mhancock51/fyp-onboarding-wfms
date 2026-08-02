6a3a03c35bfce7b514cb5d8133a51eeedf0f30bb
Matt Hancock <44118568+mhancock51@users.noreply.github.com>
2026-08-02 16:37:05 +0100

    Title rename

--- fyp-frontend/src/app/pages/WorkflowDashboardPage/WorkflowDashboardPage.tsx
+++ fyp-frontend/src/app/pages/WorkflowDashboardPage/WorkflowDashboardPage.tsx
@@ -54,7 +54,7 @@ export default function WorkflowDashboardPage() {
   const [, setPageTitle] = usePageTitle();
     
   useEffect(() => {
-    setPageTitle(`Workflow Management Dashboard`);
+    setPageTitle(`Workflow Management`);
   }, [setPageTitle]);
 
   return (
